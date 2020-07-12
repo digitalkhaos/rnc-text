dca807c6f230e9e83b4348b4675984a6e75a7c21
John DeVito <digitalkhaos@users.noreply.github.com>
2020-07-12 10:37:23 -0700

    .

--- rnc-text/Program.cs
+++ rnc-text/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace rnc_text
 {
@@ -12,7 +16,32 @@ namespace rnc_text
         }
         static void Main(string[] args)
         {
-                
+            const string path = @"c:\rnc_list";
+
+            try
+            {
+                var lines = new List<string>();
+
+                using var streamReader = new StreamReader(path, Encoding.UTF8);
+                string line;
+
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+
+                /*
+                while((line = streamReader.ReadLine()) != null)
+                {
+                    foreach(line in streamReader)
+                        NameEntry entry = new NameEntry(line);   
+                }
+                */
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 
@@ -21,10 +50,29 @@ namespace rnc_text
         private readonly int _id;
         private readonly string _name;
         private readonly int _timesChosen;
-
+        private readonly DateTime _lastChosenDateTime;
         public NameEntry(string name)
         {
+            _name = name;
+        }
 
+        public NameEntry(string name, DateTime nowDateTime)
+        {
+            _name = name;
+            _lastChosenDateTime = nowDateTime;
+            _timesChosen += 1;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public int TimeChosen
+        {
+            get;
+            set;
         }
     }
 }
